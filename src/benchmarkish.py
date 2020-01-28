120b6b3772a7808fbfdb3a85d435582699c06398
doremyr3d <francesco@calcagnini.it>
2020-01-28 16:19:36 +0100

    Almost functional prototype
    The argument parser is up and pretty much done. I had some troubles parsing the command line, so I had to resort by treating commands differently depending if I'm running on Windows or somewhere else. The logger is also there, and there's a raw print on stdout.
    The main thing that doesn't really check out is the memory usage statistic, it looks a bit too off on Windows, especially for commands that take only a couple of seconds to run.
    Now I only need total time and effective file reporting

--- src/benchmarkish.py
+++ src/benchmarkish.py
@@ -1,19 +1,55 @@
 import subprocess
 import datetime
+import sys
 import time
 import logging
 import platform
-import sys
+import shlex
 import os
-from collections import OrderedDict
+import argparse
+from collections import OrderedDict, namedtuple
+from typing import Dict, Any, List
 
 import psutil
 import openpyxl
-from openpyxl.styles import Font
-from openpyxl.worksheet.worksheet import Worksheet
+
+
+def resolve_args():
+    p = argparse.ArgumentParser(description="Command line utility that executes a command, watches over the created "
+                                            "process collecting memory and cpu usage and creates report using the "
+                                            "collected data. Everything is collected in the current working directory "
+                                            "splitting the results according to process and os, including anything "
+                                            "the command prints on stdout/stderr")
+    p.add_argument('command', type=str,
+                   help="The command to be executed. Accepts anything legal for the default system interpreter")
+    p.add_argument('-n', type=int, required=True,
+                   help="The number of times the command must be executed")
+    p.add_argument('--testname', '-t', type=str,
+                   help="Assigns a name to the test executed")
+    p.add_argument('--envname', '-e', type=str,
+                   help="Appends a custom string to the os name. e.g.: Linux_envname")
+    p.add_argument('--pname', '-p', type=str, required=True,
+                   help="Gives a custom name to the benchmark, instead of using the command's first token")
+    p.add_argument('--append', '-a', default=False, action='store_true',
+                   help="If a compatible report exists, it is extended and not replaced. Every time specifier is "
+                        "appended to the test name, and not the report file name")
+    p.add_argument('--xlsx', default=False, action='store_true',
+                   help="If specified, it generates a xlsx report file (requires openpyxl)")
+    p.add_argument('--json', default=False, action='store_true',
+                   help="If specified, it generates a json report file")
+    p.add_argument('-postcmd', type=str,
+                   help="After every run executes another command, which isn't benchmarked")
+    p.add_argument('--postfailfast', default=False, action='store_true',
+                   help="If specified, it makes the benchmark stop if the postcmd returns a non-zero value")
+    p.add_argument('--failfast', default=False, action='store_true',
+                   help="If specified, it makes the benchmark fail if the cmd returns a non-zero value")
+    p.add_argument('--environ', default=False, action='store_true',
+                   help="Dumps the environment variables of the watched process in the report")
+    return vars(p.parse_args())
 
 
 class PsRunInfo:
+    environ = {}
 
     def __init__(self, index: int):
         self.index = index
@@ -21,15 +57,32 @@ class PsRunInfo:
         self.mem_percent = []
         self.last_cpu_times = None
         self.totaltime = None
+        self.environ = None
 
     def add_cpu_perc(self, cpuperc):
         self.cpu_percent.append(cpuperc)
 
+    def avg_cpu_perc(self):
+        return sum(self.cpu_percent) / len(self.cpu_percent)
+
+    def max_cpu_perc(self):
+        return max(self.cpu_percent)
+
     def add_mem_perc(self, memperc):
         self.mem_percent.append(memperc)
 
+    def avg_mem_perc(self):
+        return sum(self.mem_percent) / len(self.mem_percent)
 
-def getsize(nbytes, suffix="B"):
+    def max_mem_perc(self):
+        return max(self.cpu_percent)
+
+    def merge_environ(self):
+        PsRunInfo.environ.update(self.environ)
+        del self.environ
+
+
+def get_size(nbytes, suffix="B"):
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
         if nbytes < factor:
@@ -39,7 +92,7 @@ def getsize(nbytes, suffix="B"):
     return f"{nbytes:.2f}{unit}{suffix}"
 
 
-def initialreport(log: logging.Logger, ws: Worksheet):
+def collect_envdata(argmap: Dict[str, Any]):
     udict = OrderedDict()
 
     # platform info
@@ -54,85 +107,127 @@ def initialreport(log: logging.Logger, ws: Worksheet):
     udict['PHYSICAL CORES'] = psutil.cpu_count(logical=False)
     udict['LOGICAL CORES'] = psutil.cpu_count(logical=True)
     udict['USAGE PERCENTAGE @STARTUP'] = psutil.cpu_percent(interval=1.0)
-    # WSL doesn't support cpu_freq, and it works like crap on windows
-    # cpufreq = psutil.cpu_freq()
-    # udict['MAX TOTAL FREQUENCY'] = f"{cpufreq.max:.2f} Mhz"
-    # udict['MIN TOTAL FREQUENCY'] = f"{cpufreq.min:.2f} Mhz"
 
     # mem info
     vmem = psutil.virtual_memory()
-    udict['TOTAL MEMORY'] = getsize(vmem.total)
-    udict['AVAILABLE MEMORY @STARTUP'] = getsize(vmem.available)
+    udict['RAW TOTAL MEMORY'] = vmem.total
+    udict['TOTAL MEMORY'] = get_size(vmem.total)
+    udict['AVAILABLE MEMORY @STARTUP'] = get_size(vmem.available)
 
     # log dump
-    log.info('='*40 + " SPECS " + '='*40)
+    logger.info('=' * 40 + " SPECS " + '=' * 40)
     for key, value in udict.items():
-        log.info(f"{key}: {value}")
-    log.info('='*87)
+        logger.info(f"{key}: {value}")
+    logger.info('=' * 87)
 
-    # wb write
-    rownum = 2
-    for key, value in udict.items():
-        label = ws.cell(rownum, 1, key)
-        label.font = Font(bold=True)
-        _ = ws.cell(rownum, 2, value)
-        rownum += 1
-
-    # FIXME Return value
-    pass
+    argmap['envstats'] = udict
 
 
-def collectdata(pid, info: PsRunInfo):
+def collectdata(pid, info: PsRunInfo, collect_environ: bool = False):
     if not pid:
-        print("nopid")
-        return
+        logger.exception("nopid")
+        return 1
 
     try:
-        print("init collect")
         pswatcher = psutil.Process(pid)
     except:
-        # FIXME collect except data
-        print("err")
-        return
+        logger.exception("Couldn't collect data")
+        return 1
     start = datetime.datetime.now()
     try:
-        # while psstatus == psutil.STATUS_RUNNING:
-        # For some weird reason WSL starts everything in sleeping, and so the loop is never executed
-        # Since that condition was never false before an exception occured on Windows, let's take a "pythonic" approach
         while 1:
             # Talking about WSL, as_dict throws KeyError there. We must take the slower approach
-            print(pswatcher.cpu_percent())
-            print(pswatcher.memory_percent())
-            print(pswatcher.cpu_times)
+            info.add_cpu_perc(pswatcher.cpu_percent())
+            info.add_mem_perc(pswatcher.memory_percent())
+            info.last_cpu_times = pswatcher.cpu_times()
+            if collect_environ:
+                info.environ = pswatcher.environ()
+
             status = pswatcher.status()
-            print(status)
             if status == psutil.STATUS_ZOMBIE:
+                # Since we're running inside the popen context, the process WILL remain [DECEASED] on Linux
+                # On Windows NoSuchProcess is thrown instead
                 break
             time.sleep(1)
     except:
-        # FIXME collect except data
-        logger.exception("interrupt")
+        logger.exception("Loop interrupted")
+
     end = datetime.datetime.now()
     info.totaltime = f"{(end - start) / datetime.timedelta(milliseconds=1)} ms"
+    return 0
 
-    pass
 
-
-def writedata(info: PsRunInfo):
-
-    pass
+def process_data(infos: List[PsRunInfo], vmem, is_environ):
+    out = OrderedDict()
+    Detail = namedtuple("Detail", ['avg_cpu', 'max_cpu', 'avg_mem', 'max_mem',
+                                   'user_cput', 'system_cput', 'child_user_cput', 'child_system_cput'])
+    details = []
+    entries = 0
+    avgcpusum = 0
+    maxcpusum = 0
+    avgmemsum = 0
+    maxmemsum = 0
+    usercputsum = 0
+    syscputsum = 0
+    cusercputsum = 0
+    csyscputsum = 0
+    for info in infos:
+        try:
+            # Extract
+            avgcpu = info.avg_cpu_perc()
+            maxcpu = info.max_cpu_perc()
+            avgmem = info.avg_mem_perc()
+            maxmem = info.max_mem_perc()
+            print(info.last_cpu_times)
+            usercput = info.last_cpu_times.user
+            syscput = info.last_cpu_times.system
+            cusercput = info.last_cpu_times.children_user
+            csyscput = info.last_cpu_times.children_system
+        except:
+            logger.exception("Processing failed. Loops continue")
+            continue
+        else:
+            # Append
+            details.append(Detail(f"{avgcpu}%", f"{maxcpu}%",
+                                  get_size(avgmem * (vmem / 100)), get_size(maxmem * (vmem / 100)),
+                                  usercput, syscput, cusercput, csyscput))
+            entries += 1
+            avgcpusum += avgcpu
+            maxcpusum += maxcpu
+            avgmemsum += avgmem
+            maxmemsum += maxmem
+            usercputsum += usercput
+            syscputsum += syscput
+            cusercputsum += cusercput
+            csyscputsum += csyscput
+            if is_environ:
+                info.merge_environ()
+    out['entries'] = entries
+    out['avg_cpu'] = f"{avgcpusum / entries}%"
+    out['max_cpu'] = f"{maxcpusum / entries}%"
+    out['avg_mem'] = get_size((vmem / 100) * (avgmemsum / entries))
+    print(get_size((vmem / 100) * (avgmemsum / entries)))
+    print(get_size((avgmemsum / entries) * (vmem / 100)))
+    out['max_mem'] = get_size((vmem / 100) * (maxmemsum / entries))
+    out['user_cput'] = usercputsum / entries
+    out['system_cput'] = syscputsum / entries
+    out['child_user_cput'] = cusercputsum / entries
+    out['child_system_cput'] = csyscputsum / entries
+    out['details'] = details
+    out['environ'] = PsRunInfo.environ
+    return out
 
 
 def log_init():
     # # logger
     out = logging.getLogger()
     out.setLevel(logging.INFO)
-    # FIXME logname
-    fh = logging.FileHandler(f'{sys.argv[0]}.{datetime.date.today().strftime("%d%m%y")}.log')
+    fh = logging.FileHandler(f'benchmarkish.{datetime.datetime.today().strftime("%d%m%yT%H%M%S")}.log')
     fh.setLevel(logging.INFO)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s -| %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     out.addHandler(fh)
@@ -142,39 +237,81 @@ def log_init():
 
 if __name__ == '__main__':
     # FIXME
-    #   - args
     #   - for loop
-    #   - stdout/stderr redirect
-    #   - logging
     #   - reporting
     #   - setup.py
 
-    # FIXME Proper args init
-    proc = sys.argv[1]
-    procname = os.path.split(proc)[1]
+    argv = resolve_args()
+    logger = log_init()
+    collect_envdata(argv)
+
+    start_time = datetime.datetime.today()
+    cmd = argv.get('command')
+    if sys.platform != 'win32':
+        cmd = list(shlex.shlex(cmd, punctuation_chars=True))
+        if cmd[0].endswith('"'):
+            cmd[0] = cmd[0].replace('"', "")
+        if cmd[0].endswith("'"):
+            cmd[0] = cmd[0].replace("'", "")
+    print(cmd)
+    procname = argv.get('pname')
+    postcmd = argv.get('postcmd')
+    if postcmd and sys.platform != 'win32':
+        postcmd = list(shlex.shlex(postcmd, punctuation_chars=True))
+
+    # create directory structure
+    folder_prefix = f"{argv['envstats']['OS']}/{procname}" if not argv['envname'] \
+        else f"{argv['envstats']['OS']}_{argv['envname']}/{procname}"
+    os.makedirs(folder_prefix, exist_ok=True)
+
+    runinfos = []
+    for i in range(0, argv['n']):
+        runinfo = PsRunInfo(i)
+        try:
+            with open(f'{folder_prefix}/{procname}.{start_time.strftime("%y%m%dT%H%M%S")}.{i}.out', mode='w') as out:
+                with subprocess.Popen(cmd, stdout=out, stderr=subprocess.STDOUT) as subp:
+                    failed = collectdata(subp.pid, runinfo, argv['environ'])
+                if failed:
+                    if argv.get('failfast'):
+                        logger.error(f"Process returned {failed}. Ending the benchmark")
+                        break
+                    else:
+                        continue
+                runinfos.append(runinfo)
+                if postcmd:
+                    out.write('='*39 + " POSTCMD " + '='*39 + '\n')
+                    runret = subprocess.run(postcmd, stdout=out, stderr=subprocess.STDOUT)
+                    if argv.get('postfailfast') and runret.returncode:
+                        logger.error(f"Post command returned {runret.returncode}. Ending the benchmark")
+                        break
+        except:
+            logger.exception("Can't open the process")
+
+    report = process_data(runinfos, argv['envstats']['RAW TOTAL MEMORY'], argv['environ'])
+    print(report)
+
+
     # FIXME split dumplog and dumpxlsx - better to work on the sheet at the end then this split thing
     # Turns out that machine type and architecture are meaningless stats to get. You have to hope that in the
     # process environ is stored the architecture if you want that info
     # The side note is, shared drives do exist, so it makes sense to register the system name
-    system_name = platform.uname().system
-    folder_prefix = f"{system_name}/{procname}"
-    os.makedirs(folder_prefix, exist_ok=True)
+    # system_name = platform.uname().system
+    # folder_prefix = f"{system_name}/{procname}"
+    # os.makedirs(folder_prefix, exist_ok=True)
 
-    # logger
-    logger = log_init()
-    # FIXME add time
-    wb_name = folder_prefix + f"/{procname}.{datetime.date.today().strftime('%d%m%y')}.xlsx"
-    workbook = openpyxl.Workbook()
-    if workbook.active:
-        ws_summary = workbook.active
-        ws_summary.title = "SUMMARY"
-    else:
-        ws_summary = workbook.create_sheet("SUMMARY")
-    initialreport(logger, ws_summary)
-
-    with subprocess.Popen(sys.argv[1]) as subp:
-        runinfo = PsRunInfo(0)
-        collectdata(subp.pid, runinfo)
-    writedata(runinfo)
-    workbook.save(wb_name)
+    # # FIXME add time
+    # wb_name = folder_prefix + f"/{procname}.{datetime.date.today().strftime('%d%m%y')}.xlsx"
+    # workbook = openpyxl.Workbook()
+    # if workbook.active:
+    #     ws_summary = workbook.active
+    #     ws_summary.title = "SUMMARY"
+    # else:
+    #     ws_summary = workbook.create_sheet("SUMMARY")
+    # initialreport(logger, ws_summary)
+    #
+    # with subprocess.Popen(sys.argv[1]) as subp:
+    #     runinfo = PsRunInfo(0)
+    #     collectdata(subp.pid, runinfo)
+    # writedata(runinfo)
+    # workbook.save(wb_name)
     print("over")
